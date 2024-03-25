0193cd51fda42120cd934819466ddb10d150f805
Salavatulin <v.salavatulin@gmail.com>
2024-03-25 15:06:35 +0500

    Стартовое состояние.

---
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Салаватулин Владимир on 25.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
